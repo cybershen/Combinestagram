619b7789d51b86c2580b1dc2042b7e7ae1561068
cybershen <cyberpuer@proton.me>
2023-01-20 17:48:26 +0200

    Initial Commit

---
+++ RxSwiftTestProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftTestProj
+//
+//  Created by Назар Жиленко on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
